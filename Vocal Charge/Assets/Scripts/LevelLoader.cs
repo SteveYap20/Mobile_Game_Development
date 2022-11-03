e963b69cc328f0ce97b6e4aaa86de2ecf0248d41
SteveYap20 <Hou.Yap@mail.bcu.ac.uk>
2022-11-03 07:05:48 +0000

    Menu UI addition & Transition ongoing

---
+++ Vocal Charge/Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour {
+
+    public Animator transition;
+    public float transitionTime = 1.0f;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0)) {
+
+            LoadNextLevel();
+        }
+
+    }
+
+    public void LoadNextLevel() {
+
+        StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
+    }
+
+    //Corountine
+    IEnumerator LoadLevel(int levelIndex) {
+
+        //Activate animation
+        transition.SetTrigger("Start");
+
+        //Waiting time
+        yield return new WaitForSeconds(transitionTime);
+
+        //Loading scene
+        SceneManager.LoadScene(levelIndex);
+    }
+}
--- Vocal Charge/Assets/Scripts/PauseMenu.cs
+++ Vocal Charge/Assets/Scripts/PauseMenu.cs
@@ -1,40 +1,60 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class PauseMenu : MonoBehaviour {
+public class PauseMenu : MonoBehaviour
+{
 
     public static bool GamePaused = false;
 
     public GameObject pauseMenuUI;
 
     // Update is called once per frame
-    void Update() {
+    void Update()
+    {
 
-        if (Input.GetKeyDown(KeyCode.Escape)) {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
             if (GamePaused)
             {
                 Resume();
             }
-            else {
+            else
+            {
                 Pause();
             }
         }
 
     }
 
-    void Resume() {
+    public void Resume()
+    {
 
         pauseMenuUI.SetActive(false);
         Time.timeScale = 1.0f;
         GamePaused = false;
     }
 
-    void Pause() {
+    void Pause()
+    {
 
         pauseMenuUI.SetActive(true);
         Time.timeScale = 0.0f;
         GamePaused = true;
+    }
+
+    public void LoadMenu()
+    {
+
+        Time.timeScale = 1.0f;
+        SceneManager.LoadScene("Menu Page");
+    }
+
+    public void QuitGame()
+    {
+
+        Debug.Log("Quitting Game . . . ");
     }
 
 }
